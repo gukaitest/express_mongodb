e37d8ccc8b7bf0f9bf98abb26737c572499d9ce8
gukai <1036350534@qq.com>
2025-06-09 14:33:56 +0800

    fix: bug修复

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // 只连接商品数据库
 // 使用环境变量或默认值（开发环境）
-const dbUrl = process.env.DB_URL || 'mongodb://gukai:Gk324376@172.17.0.1:27017/product_info';
+const dbUrl = process.env.DB_URL || 'mongodb://172.17.0.1:27017/product_info';
 mongoose.connect(dbUrl);
 // mongoose.connect('mongodb://localhost:27017/product_info', {
 // //   useNewUrlParser: true,
