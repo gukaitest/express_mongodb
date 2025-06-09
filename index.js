37824668e47474545a787d5c80f6e75e50e2ecca
gukai <1036350534@qq.com>
2025-06-09 17:20:51 +0800

    feat: 添加跨域允许规则

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = 3000;
 // 初始化配置
 // connectDB();
 // app.use(express.json());
-// app.use(cors);
+app.use(cors);
 // app.use(cors({
 //   origin: 'http://47.103.169.121:8083', // 仅限前端域名
 //   credentials: true
