15d2ff356eeff3d7f566f22d9d7028b4dc589e35
gukai <1036350534@qq.com>
2025-06-09 19:08:16 +0800

    fix: 修改连接参数

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // 只连接商品数据库
 // 使用环境变量或默认值（开发环境）
 // const dbUrl = process.env.DB_URL || 'mongodb://gukai:Gk324376@172.17.0.1:27017/product_info';
-const dbUrl = process.env.DB_URL || 'mongodb://gukai:Gk324376@172.17.0.1:27017/product_info';
+const dbUrl = process.env.DB_URL || 'mongodb://gukai:Gk324376@172.17.0.1:27017/product_info?authSource=admin';
 mongoose.connect(dbUrl);
 // mongoose.connect('mongodb://localhost:27017/product_info', {
 // //   useNewUrlParser: true,
